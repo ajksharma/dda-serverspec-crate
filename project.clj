d22706ccd45a9928faaf0f6d46d76e8b307c4ee5
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-12-29 16:05:12 +0100

    updated deps

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [prismatic/schema "1.1.3"]
-                 [ch.qos.logback/logback-classic "1.1.7"]
+                 [ch.qos.logback/logback-classic "1.1.8"]
                  [com.palletops/pallet "0.8.12"]
-                 [org.domaindrivenarchitecture/dda-pallet-commons "0.2.2-SNAPSHOT"]
-                 [org.domaindrivenarchitecture/dda-pallet "0.2.0"]                 
+                 [org.domaindrivenarchitecture/dda-pallet-commons "0.3.0"]
+                 [org.domaindrivenarchitecture/dda-pallet "0.3.0"]                 
                  ]
   :source-paths ["src" "test-utils"]
   :repositories [["snapshots" :clojars]
@@ -21,15 +21,15 @@
               [[org.clojure/test.check "0.9.0"]
                [com.palletops/stevedore "0.8.0-beta.7"]
                [com.palletops/pallet "0.8.12" :classifier "tests"]
-               [org.domaindrivenarchitecture/dda-user-crate "0.3.4"]
-               [org.domaindrivenarchitecture/dda-init-crate "0.2.0"]
+               [org.domaindrivenarchitecture/dda-user-crate "0.3.5-SNAPSHOT"]
+               [org.domaindrivenarchitecture/dda-init-crate "0.2.1-SNAPSHOT"]
                [org.domaindrivenarchitecture/pallet-aws "0.2.8-SNAPSHOT"]
-               [ch.qos.logback/logback-classic "1.1.7"]
-               [org.slf4j/jcl-over-slf4j "1.7.21"]
+               [ch.qos.logback/logback-classic "1.1.8"]
+               [org.slf4j/jcl-over-slf4j "1.7.22"]
               ]}
              :plugins [[lein-sub "0.3.0"]]
              :leiningen/reply
-             {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.21"]]
+             {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.22"]]
               :exclusions [commons-logging]}}
   :local-repo-classpath true
   :classifiers {:tests {:source-paths ^:replace ["test" "integration"]
