ed930cc1391f0057241c8547ab1383424b86eed9
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-05-12 15:31:36 +0200

    fixed package name for tests

--- src/dda/pallet/crate/servertest/test/netstat.clj
+++ src/dda/pallet/crate/servertest/test/netstat.clj
@@ -14,7 +14,7 @@
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
 
-(ns dda.pallet.servertest.test.netstat
+(ns dda.pallet.crate.servertest.test.netstat
   (:require
     [schema.core :as s]
     [org.domaindrivenarchitecture.pallet.servertest.fact.netstat :as netstat-fact]
--- src/dda/pallet/crate/servertest/test/packages.clj
+++ src/dda/pallet/crate/servertest/test/packages.clj
@@ -13,7 +13,7 @@
 ; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
-(ns dda.pallet.servertest.test.packages
+(ns dda.pallet.crate.servertest.test.packages
   (:require
     [schema.core :as s]
     [org.domaindrivenarchitecture.pallet.servertest.core.test :as server-test]
