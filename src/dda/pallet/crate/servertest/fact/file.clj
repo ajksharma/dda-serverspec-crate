9a9440d46c7b6d970eeb023077422c542f5a453e
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-05-12 11:20:49 +0200

    added some todos

--- src/dda/pallet/crate/servertest/fact/file.clj
+++ src/dda/pallet/crate/servertest/fact/file.clj
@@ -18,32 +18,35 @@
   (:require
     [dda.pallet.servertest.core.fact :refer :all]))
 
+; todo: create crate boundary & schema for configuration & result
+
 (def fact-id-netstat ::file)
 
 (def FileFactConfig {:file-paths [s/Str]})
 
-(def FileFactResult {s/Keyword {:exist s/Bool
-                                :directory s/Bool
-                                :link s/Bool
-                                :owner s/Str
+(def FileFactResult {s/Keyword {:size-in-bytes s/Num
+                                :user s/Str
                                 :group s/Str
                                 :mod s/Str
-                                :size s/Num
-                                :}})
-
-
+                                :type s/Str ;use keywords instead here?
+                                :created s/Str
+                                :modified s/Str
+                                :accessed s/Str}})
 
 (defn parse-find
   [file-resource]
+  ;todo create parse function
   (map #(zipmap
-          [:name :size :user :group :mod :type :created :modified :accessed]
+          [:name :size-in-bytes :user :group :mod :type :created :modified :accessed]
           (clojure.string/split (clojure.string/trim %) #"\s+|/"))
      (drop-while #(not (re-matches #"\s*(tcp|udp).*" %))
        (clojure.string/split netstat-resource #"\n"))))
 
 (s/defn collect-netstat-fact
   "Defines the netstat resource.
-   This is automatically done serverstate crate is used."
+   This is automatically done serverstate crate is used.
+   See also: https://unix.stackexchange.com/questions/128985/why-not-parse-ls, http://man7.org/linux/man-pages/man1/find.1.html"
   [files-to-inspect :- FileFactConfig]
   ;find linked-entry -prune -printf "'%f' '%s' '%u' '%g' '%m' '%y' '%c' '%t' '%a'\n"
+  ;todo: iterate over files-to-inspect & create one script
   (collect-fact fact-id-netstat '("find" ~path "-prune" "-printf \"'%f' '%s' '%u' '%g' '%m' '%y' '%c' '%t' '%a'\n\"") :transform-fn parse-find))
