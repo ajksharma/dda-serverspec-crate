30924739110fba0ee77f0caf0dc36bcbc39ba1be
az <az@mydomain>
2018-02-09 18:58:35 +0100

    added http fact and test

--- main/src/dda/pallet/dda_serverspec_crate/infra/fact/http.clj
+++ main/src/dda/pallet/dda_serverspec_crate/infra/fact/http.clj
@@ -14,33 +14,34 @@
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
 
-(ns dda.pallet.dda-serverspec-crate.infra.fact.certificate
+(ns dda.pallet.dda-serverspec-crate.infra.fact.http
   (:require
    [schema.core :as s]
    [clojure.string :as string]
    [dda.pallet.dda-serverspec-crate.infra.core.fact :refer :all]))
 
 ; -----------------------  fields & schemas  ------------------------
-(def fact-id-certificate ::certificate)
+(def fact-id-http ::http)
 
-(def CertificateFactConfig {s/Keyword {:file s/Str}})  ;with full path
+(def HttpFactConfig
+  {s/Keyword {:url s/Str}})         ;e.g. http://google.com
 
-(def CertificateFactResult {:expiration-days s/Num})   ;value -1 in case of error (e.g. missing file)
+(def HttpFactResult {:expiration-days s/Num})    ;value -1 in case of error
 
-(def CertificateFactResults {s/Keyword CertificateFactResult})
+(def HttpFactResults {s/Keyword HttpFactResult})
 
-(def output-separator "----- certificate output separator -----\n")
+(def output-separator "----- http output separator -----\n")
 
 ; -----------------------  functions  -------------------------------
-(s/defn certificate-file-to-keyword :- s/Keyword
-  "creates a keyword from the certificate file-path"
-  [file :- s/Str] (keyword (clojure.string/replace file #"/" "_")))
+(s/defn url-to-keyword :- s/Keyword
+  "creates a keyword from the url"
+  [url :- s/Str] (keyword (clojure.string/replace url #"/" "_")))
 
-(s/defn build-certificate-script
-  "builds the script to check the expiration days certificate"
-  [certificate-config :- CertificateFactConfig]
-  (let [config-val (val certificate-config)
-        config-key (key certificate-config)
+(s/defn build-http-script
+  "builds the script to retrieve the http response of the url"
+  [http-config :- HttpFactConfig]
+  (let [config-val (val http-config)
+        config-key (key http-config)
         {:keys [file]} config-val]
     (str
       "echo '" (name config-key) "';"
@@ -49,8 +50,8 @@
       " -noout -enddate | cut -d= -f 2)\" \"+%s\") - $(date \"+%s\") ) / 86400));"
       "echo -n '" output-separator "'")))
 
-(s/defn parse-certificate-response :- CertificateFactResult
-  "returns a CertificateFactResult from the result text of one certificate check"
+(s/defn parse-http-response :- HttpFactResult
+  "returns a HttpFactResult from the result text of one http check"
   [single-script-result]
   (let [result-lines (string/split single-script-result #"\n")
         result-key (first result-lines)
@@ -61,19 +62,19 @@
       {(keyword result-key) {:expiration-days (Integer. result-number)}}
       {(keyword result-key) {:expiration-days -1}})))
 
-(defn parse-certificate-script-responses
-  "returns a CertificateFactResult from the result text of one certificate check"
+(defn parse-http-script-responses
+  "returns a HttpFactResult from the result text of one http check"
   [raw-script-results]
   (apply merge
-    (map parse-certificate-response (clojure.string/split raw-script-results (re-pattern output-separator)))))
+    (map parse-http-response (clojure.string/split raw-script-results (re-pattern output-separator)))))
 
-(s/defn collect-certificate-fact
-  "Collects the facts for the certificates using the script"
-  [fact-config :- CertificateFactConfig]
+(s/defn collect-http-fact
+  "Collects the facts for all http checks by executing the script and parsing the results"
+  [fact-config :- HttpFactConfig]
   (collect-fact
-    fact-id-certificate
+    fact-id-http
     (str
       (clojure.string/join
-       "; " (map #(build-certificate-script %) fact-config))
+       "; " (map #(build-http-script %) fact-config))
       "; exit 0")
-    :transform-fn parse-certificate-script-responses))
+    :transform-fn parse-http-script-responses))
