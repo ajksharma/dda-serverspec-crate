7cddfe23bb3ffe5d91b4b531008b4327028ea5a4
jem <michael.jerger@meissa-gmbh.de>
2018-02-09 14:47:20 +0100

    fix logging for uberjar

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [dda/dda-pallet "1.0.2-SNAPSHOT"]
-                 [org.clojure/tools.cli "0.3.5"]]
+                 [dda/dda-pallet "1.0.2-SNAPSHOT"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -20,6 +19,7 @@
                    :dependencies
                    [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
                     [com.palletops/pallet "0.8.12" :classifier "tests"]
+                    [org.clojure/tools.cli "0.3.5"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha2"]
                     [org.slf4j/jcl-over-slf4j "1.8.0-beta1"]]
                    :plugins [[lein-sub "0.3.0"]
@@ -34,5 +34,7 @@
                        :resource-paths ["uberjar/resources"]
                        :aot :all
                        :main dda.pallet.dda-serverspec-crate.main
-                       :dependencies [[org.clojure/tools.cli "0.3.5"]]}}
+                       :dependencies [[org.clojure/tools.cli "0.3.5"]
+                                      [ch.qos.logback/logback-classic "1.3.0-alpha2"]
+                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta1"]]}}
   :local-repo-classpath true)
