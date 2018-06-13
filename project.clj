3dac9ed48bfd7a81406c47fd7302a4a2d7fa2243
jem <michael.jerger@meissa-gmbh.de>
2018-06-13 13:54:00 +0200

    Version 1.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-serverspec-crate "1.1.0"
+(defproject dda/dda-serverspec-crate "1.1.1-SNAPSHOT"
   :description "A crate to get facts from server nodes and test these facst against your expectation."
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
