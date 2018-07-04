0333eefe9e1d52e32ee736f67f9097d40bf859f8
jem <michael.jerger@meissa-gmbh.de>
2018-07-04 17:46:17 +0200

    updated deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[dda/dda-pallet "2.2.0"]]
+  :dependencies [[dda/dda-pallet "2.2.1"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
