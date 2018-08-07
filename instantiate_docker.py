b379338ed4e96790033b59250fb68586aa56ca7d
gec <clemens.geibel@meissa-gmbh.de>
2018-08-07 18:24:23 +0200

    Updated Doc

--- instantiate_docker.py
+++ instantiate_docker.py
@@ -5,8 +5,8 @@ import os
 # Please perform the following steps in order to use this script
 # 1) Install pyton 3 and pip3: sudo apt install python3-pip python3
 # 2) Install the docker sdk with pip: pip3 install docker
-# 3) Make sure you have the ubuntu image: docker image pull ubuntu
-# 4) Build the image ubuntu-test from the Dockerfile in the repo.
+# 3) TODO: Check image openjdk:8 for using ubuntu
+# 4) TODO: Make it possible to specify docker container cmd per command line (Default java -jar...)
 
 # TODO add cli documentation
 
@@ -15,8 +15,9 @@ docker_logs = os.getcwd() + '/docker-logs/'
 if not os.path.exists(docker_logs):
     os.makedirs(docker_logs)
 
-edn_file = '/home/krj/repo/dda-pallet/dda-serverspec-crate/' + sys.argv[1]
-jar_file = '/home/krj/repo/dda-pallet/dda-serverspec-crate/' + sys.argv[2]
+# TODO relative path (os.getcwd())
+edn_file = '/home/gec/repo/dda-pallet/dda-serverspec-crate/' + sys.argv[1]
+jar_file = '/home/gec/repo/dda-pallet/dda-serverspec-crate/' + sys.argv[2]
 
 execute_command = 'java -jar /app/uberjar.jar /app/config.edn'
 
@@ -26,18 +27,18 @@ print(debug_map)
 client = docker.APIClient()
 
 container = client.create_container(
-    image='ubuntu-test:latest',
+    image='openjdk:8',
     command='java -jar /app/uberjar.jar /app/config.edn',
     volumes=['/app/config.edn', '/app/uberjar.jar', '/logs'],
 
     host_config=client.create_host_config(binds={
         edn_file: {
             'bind': '/app/config.edn',
-            'mode': 'rw',
+            'mode': 'ro',
         },
         jar_file: {
             'bind': '/app/uberjar.jar',
-            'mode': 'rw',
+            'mode': 'ro',
         },
         docker_logs: {
             'bind': '/logs/',
