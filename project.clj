4cdf94268c174c1b9a514eb4df057072ac6ef09c
lukas <l.schondorff@gmail.com>
2018-08-10 11:17:56 +0200

    Version 1.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-serverspec-crate "1.2.1-SNAPSHOT"
+(defproject dda/dda-serverspec-crate "1.2.1"
   :description "A crate to get facts from server nodes and test these facst against your expectation."
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
